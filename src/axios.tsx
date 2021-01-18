cb4654185528361a87634614ff6ae84cf4135c5e
KJ <884485m@gmail.com>
2021-01-18 19:54:28 +0200

    Update to API call

--- src/axios.tsx
+++ src/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 
 const BASE_URL =
-  "https://api.rawg.io/api/games?key=5c13465251ea47dcab0740521d5da4e8&dates=2020-01-01,2020-12-31&ordering=-rating&page=";
+  "https://api.rawg.io/api/games?key=5c13465251ea47dcab0740521d5da4e8&dates=2020-01-01,2020-12-31&platforms=18&ordering=-rating&page=";
 
 export default function Http(page: Number) {
   const [responseData, setResponseData] = React.useState<any>([]);
